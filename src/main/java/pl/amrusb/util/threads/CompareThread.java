24346b32240b2c67d5d654a8243819aaa98a5475
Bartosz Surma <bartosz.surma@outlook.com>
2024-02-23 17:18:16 +0100

    Uzupelnianie w tabelach statystyk dla porownania

--- src/main/java/pl/amrusb/util/threads/CompareThread.java
+++ src/main/java/pl/amrusb/util/threads/CompareThread.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import pl.amrusb.algs.seg.*;
 import pl.amrusb.algs.seg.imp.KMeans;
 import pl.amrusb.algs.seg.weka.WekaKMeans;
+import pl.amrusb.util.models.Cluster;
+import pl.amrusb.util.models.Point3D;
 import pl.amrusb.util.ui.MainFrame;
 import pl.amrusb.util.ui.MainMenuBar;
 
@@ -14,6 +16,8 @@ import pl.amrusb.util.ui.panels.ImagePanel;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Map;
 
 @AllArgsConstructor
 public class CompareThread extends Thread{
@@ -34,12 +38,14 @@ public class CompareThread extends Thread{
         segmentation = new KMeans(clusterNum, current.getOriginalImage());
         segmentation.execute();
 
+        Map<AKMeans.KMeansStats, Object> ownStats = segmentation.getStatistics();
+
         leftImage = segmentation.getOutputImage();
-
-
 
         segmentation = new WekaKMeans(clusterNum, current.getOriginalImage());
         segmentation.execute();
+
+        Map<AKMeans.KMeansStats, Object> wekaStats = segmentation.getStatistics();
 
         rightImage = segmentation.getOutputImage();
 
@@ -55,5 +61,26 @@ public class CompareThread extends Thread{
 
         action.setEnabled(true);
         MainMenuBar.getOwner().setCursor(Cursor.getDefaultCursor());
+        BottomPanel.setProgressBarVisible(false);
+        current.getComparePanel().addPropTableRow("<html><b>Indeks Jaccard'a</b></html>", "TBD", "TBD");
+        current.getComparePanel().addPropTableRow("<html><b>Współczynnik Dice’a</b></html>", "TBD", "TBD");
+
+        current.getComparePanel().addPropTableRow(
+                "<html><b>Liczba iteracji</b></html>",
+                ownStats.get(AKMeans.KMeansStats.ITERATIONS),
+                wekaStats.get(AKMeans.KMeansStats.ITERATIONS));
+        current.getComparePanel().addPropTableRow(
+                "<html><b>Czas trwania</b></html>",
+                ownStats.get(AKMeans.KMeansStats.TIME),
+                wekaStats.get(AKMeans.KMeansStats.TIME));
+        current.getComparePanel().fillClustersTable(
+                (ArrayList<Cluster>) ownStats.get(AKMeans.KMeansStats.CLUSTER_CENTROIDS),
+                (ArrayList<Cluster>)wekaStats.get(AKMeans.KMeansStats.CLUSTER_CENTROIDS)
+        );
+
+        current.getComparePanel().fillInitialsTable(
+                (ArrayList<Point3D>) ownStats.get(AKMeans.KMeansStats.INITIAL_START_POINTS),
+                (ArrayList<Point3D>) wekaStats.get(AKMeans.KMeansStats.INITIAL_START_POINTS)
+        );
     }
 }
--- src/main/java/pl/amrusb/util/ui/panels/ComparePanel.java
+++ src/main/java/pl/amrusb/util/ui/panels/ComparePanel.java
@@ -6,11 +6,16 @@ import lombok.Setter;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import pl.amrusb.util.img.ImageRescaler;
+import pl.amrusb.util.models.Cluster;
+import pl.amrusb.util.models.Point3D;
 import pl.amrusb.util.ui.MainFrame;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 @Getter
 @Setter
@@ -36,13 +41,23 @@ public class ComparePanel extends JPanel {
     private JFreeChart imageChart;
     private ChartPanel leftBottomPanel;
 
+    private JPanel statsPanel;
+    private CardLayout cardLayout;
+    private JTable propTable;
+    private DefaultTableModel propModel;
+
+    private DefaultTableModel ownCModel;
+    private JTable ownCTable;
+    private DefaultTableModel wekaCModel;
+    private JTable wekaCTable;
+
+    private DefaultTableModel ownIModel;
+    private JTable ownITable;
+    private DefaultTableModel wekaIModel;
+    private JTable wekaITable;
+
     private JLabel lImageName;
     private JLabel lClusterNum;
-    private JLabel lRsme;
-    private JLabel lJackard;
-    private JLabel lDice;
-    private JLabel lOwnTime;
-    private JLabel lWekaTime;
 
     public ComparePanel(){
         setLayout(new BorderLayout());
@@ -107,13 +122,13 @@ public class ComparePanel extends JPanel {
 
     private void createBottomPanel(){
         GridBagConstraints c = new GridBagConstraints();
-        c.weightx = 100;
-        c.weighty = 100;
+        c.weightx = 50;
+        c.weighty = 50;
         c.gridx = 0;
         c.gridy = 0;
-        c.gridwidth = 4;
-        c.anchor = GridBagConstraints.CENTER;
-        c.fill = GridBagConstraints.NONE;
+        c.gridwidth = 1;
+        c.anchor = GridBagConstraints.EAST;
+        c.fill = GridBagConstraints.HORIZONTAL;
         c.insets.set(20, 10, 5, 10);
 
         bottomPanel = new JPanel();
@@ -130,69 +145,204 @@ public class ComparePanel extends JPanel {
         JPanel rightBottomPanel = new JPanel();
         rightBottomPanel.setLayout(new GridBagLayout());
 
+        rightBottomPanel.add(new JLabel("Plik:"), c);
+        c.gridx = 1;
+        c.anchor = GridBagConstraints.WEST;
         lImageName = new JLabel();
         rightBottomPanel.add(lImageName, c);
-        c.gridx = 0;
-        c.gridy = 1;
-        c.gridwidth = 2;
+        c.gridx = 2;
         c.anchor = GridBagConstraints.EAST;
         c.insets.set(5, 10, 5, 10);
-        rightBottomPanel.add(new JLabel("<html><b>Liczba klastrów:</b></html>"), c);
-        c.gridx = 2;
+        rightBottomPanel.add(new JLabel("Liczba klastrów:"), c);
+
+        c.gridx = 3;
         c.anchor = GridBagConstraints.WEST;
         lClusterNum = new JLabel();
         rightBottomPanel.add(lClusterNum, c);
-        c.gridx = 0;
-        c.gridy = 2;
-        c.anchor = GridBagConstraints.EAST;
-        rightBottomPanel.add(new JLabel("<html><b>RMSE:</b></html>"), c);
-        c.gridx = 2;
-        c.anchor = GridBagConstraints.WEST;
-        lRsme = new JLabel("X");
-        rightBottomPanel.add(lRsme, c);
 
-        c.gridx = 0;
-        c.gridy = 3;
-        c.anchor = GridBagConstraints.EAST;
-        rightBottomPanel.add(new JLabel("<html><b>Indeks Jaccard'a:</b></html>"), c);
-        c.gridx = 2;
-        c.anchor = GridBagConstraints.WEST;
-        lJackard = new JLabel("X");
-        rightBottomPanel.add(lJackard, c);
-
-        c.gridx = 0;
-        c.gridy = 4;
-        c.anchor = GridBagConstraints.EAST;
-        rightBottomPanel.add(new JLabel("<html><b>Współczynnik Dice’a:</b></html>"), c);
-        c.gridx = 2;
-        c.anchor = GridBagConstraints.WEST;
-        lDice = new JLabel("X");
-        rightBottomPanel.add(lDice, c);
-
-        c.gridx = 0;
-        c.gridy = 5;
-        c.gridwidth = 4;
-        c.anchor = GridBagConstraints.CENTER;
-        rightBottomPanel.add(new JLabel("<html><b>Czas trwania algorytmów:</b></html>"), c);
-        c.gridy = 6;
-        c.gridwidth = 1;
-        c.anchor = GridBagConstraints.EAST;
-        c.insets.set(5, 10, 20, 10);
-        rightBottomPanel.add(new JLabel("<html><b>Implementacja:</b></html>"), c);
-        c.gridx = 1;
-        c.anchor = GridBagConstraints.WEST;
-        lOwnTime = new JLabel("XXX");
-        rightBottomPanel.add(lOwnTime, c);
-        c.gridx = 2;
-        c.anchor = GridBagConstraints.EAST;
-        rightBottomPanel.add(new JLabel("<html><b>Weka:</b></html>"), c);
         c.gridx = 4;
+        c.anchor = GridBagConstraints.EAST;
+        rightBottomPanel.add(new JLabel("Pokaż statystyki:"), c);
+
+        c.gridx = 5;
         c.anchor = GridBagConstraints.WEST;
-        lWekaTime = new JLabel("XXX");
-        rightBottomPanel.add(lWekaTime, c);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        JComboBox<String> comboBox =  StatsComboBox.getJComboBox();
+        comboBox.addItemListener(e->{
+            cardLayout.show(statsPanel, e.getItem().toString());
+        });
+        rightBottomPanel.add(comboBox, c);
+
+        c.gridx = 0;
+        c.gridy = 1;
+        c.fill = GridBagConstraints.BOTH;
+        c.gridwidth = 6;
+        c.gridheight = 2;
+
+        createStatsPanel();
+        rightBottomPanel.add(statsPanel, c);
+
 
         bottomPanel.add(leftBottomPanel);
         bottomPanel.add(rightBottomPanel);
+    }
+
+    private void createStatsPanel(){
+        cardLayout = new CardLayout();
+        statsPanel = new JPanel();
+        statsPanel.setLayout(cardLayout);
+
+        JPanel metricsPanel = new JPanel();
+        metricsPanel.setLayout(new BorderLayout());
+
+        propModel = new DefaultTableModel();
+        propTable = new JTable(propModel);
+
+        propModel.addColumn("Właściwość");
+        propModel.addColumn("Implementacja");
+        propModel.addColumn("Weka");
+
+        propTable.setAutoCreateRowSorter(true);
+        metricsPanel.add(new JScrollPane(propTable), BorderLayout.CENTER);
+
+
+        JPanel clustersPanel  = new JPanel();
+        clustersPanel.setLayout(new BorderLayout());
+
+        ownCModel = new DefaultTableModel();
+        ownCTable = new JTable(ownCModel);
+        ownCTable.setAutoCreateRowSorter(true);
+
+        ownCModel.addColumn("Lp.");
+        ownCModel.addColumn("Współrzędne");
+        ownCModel.addColumn("Rozmiar");
+        ownCModel.addColumn("Kolor");
+
+        wekaCModel = new DefaultTableModel();
+        wekaCTable = new JTable(wekaCModel);
+        wekaCTable.setAutoCreateRowSorter(true);
+
+        wekaCModel.addColumn("Lp.");
+        wekaCModel.addColumn("Współrzędne");
+        wekaCModel.addColumn("Rozmiar");
+        wekaCModel.addColumn("Kolor");
+
+        clustersPanel.add(new JScrollPane(ownCTable), BorderLayout.WEST);
+        clustersPanel.add(new JScrollPane(wekaCTable), BorderLayout.EAST);
+
+
+        JPanel initialsPanel = new JPanel();
+        initialsPanel.setLayout(new BorderLayout());
+        ownIModel = new DefaultTableModel();
+        ownITable = new JTable(ownIModel);
+        ownITable.setAutoCreateRowSorter(true);
+
+        ownIModel.addColumn("Lp.");
+        ownIModel.addColumn("Współrzędne");
+        ownIModel.addColumn("Kolor");
+
+        wekaIModel = new DefaultTableModel();
+        wekaITable = new JTable(wekaIModel);
+        wekaITable.setAutoCreateRowSorter(true);
+
+        wekaIModel.addColumn("Lp.");
+        wekaIModel.addColumn("Współrzędne");
+        wekaIModel.addColumn("Kolor");
+
+        initialsPanel.add(new JScrollPane(ownITable), BorderLayout.WEST);
+        initialsPanel.add(new JScrollPane(wekaITable), BorderLayout.EAST);
+
+        statsPanel.add(metricsPanel, StatsComboBox.PROPERTIES.value);
+        statsPanel.add(new JScrollPane(clustersPanel), StatsComboBox.CLUSTERS.value);
+        statsPanel.add(new JScrollPane(initialsPanel), StatsComboBox.INITIAL.value);
+
+    }
+    public void fillInitialsTable(ArrayList<Point3D> dataSet1, ArrayList<Point3D> dataSet2){
+        String color = "<html><body style=\"background-color:%s;\"> %s </body></html>";
+        if(dataSet1.size() == dataSet2.size()){
+            for (int i = 0; i < dataSet1.size();i++) {
+                Point3D ds1C = dataSet1.get(i);
+                Point3D ds2C = dataSet2.get(i);
+
+                ownIModel.addRow(new Object[]{
+                        i + 1,
+                        ds1C.toString(),
+                        String.format(color, ds1C.toHex(), ds1C.toHex())
+                });
+                wekaIModel.addRow(new Object[]{
+                        i + 1,
+                        ds2C.toString(),
+                        String.format(color, ds2C.toHex(), ds2C.toHex())
+                });
+            }
+        }
+        else{
+            for (int i = 0; i < dataSet1.size();i++) {
+                Point3D ds1C = dataSet1.get(i);
+
+                ownIModel.addRow(new Object[]{
+                        i + 1,
+                        ds1C.toString(),
+                        String.format(color, ds1C.toHex(), ds1C.toHex())
+                });
+            }
+            for (int i = 0; i < dataSet2.size();i++) {
+                Point3D ds2C = dataSet2.get(i);
+
+                wekaIModel.addRow(new Object[]{
+                        i + 1,
+                        ds2C.toString(),
+                        String.format(color, ds2C.toHex(), ds2C.toHex())
+                });
+            }
+        }
+    }
+    public void fillClustersTable(ArrayList<Cluster> dataSet1, ArrayList<Cluster> dataSet2){
+        String color = "<html><body style=\"background-color:%s;\"> %s </body></html>";
+        if(dataSet1.size() == dataSet2.size()){
+            for (int i = 0; i < dataSet1.size();i++) {
+                Cluster ds1C = dataSet1.get(i);
+                Cluster ds2C = dataSet2.get(i);
+
+                ownCModel.addRow(new Object[]{
+                        i + 1,
+                        ds1C.toString(),
+                        ds1C.getSize(),
+                        String.format(color, ds1C.toHex(), ds1C.toHex())
+                });
+                wekaCModel.addRow(new Object[]{
+                        i + 1,
+                        ds2C.toString(),
+                        ds2C.getSize(),
+                        String.format(color, ds2C.toHex(), ds2C.toHex())
+                });
+            }
+        }
+        else{
+            for (int i = 0; i < dataSet1.size();i++) {
+                Cluster ds1C = dataSet1.get(i);
+
+                ownCModel.addRow(new Object[]{
+                        i + 1,
+                        ds1C.toString(),
+                        ds1C.getSize(),
+                        String.format(color, ds1C.toHex(), ds1C.toHex())
+                });
+            }
+            for (int i = 0; i < dataSet2.size();i++) {
+                Cluster ds2C = dataSet2.get(i);
+
+                wekaCModel.addRow(new Object[]{
+                        i + 1,
+                        ds2C.toString(),
+                        ds2C.getSize(),
+                        String.format(color, ds2C.toHex(), ds2C.toHex())
+                });            }
+        }
+    }
+
+    public void addPropTableRow(Object val1, Object val2, Object val3){
+        propModel.addRow(new Object[]{val1, val2, val3});
     }
 
     public void setImageLabel(BufferedImage image, Position position){
@@ -229,27 +379,27 @@ public class ComparePanel extends JPanel {
         leftBottomPanel.setChart(chart);
     }
 
-    public void setRSME(String value){
-        lRsme.setText(value);
-    }
-    public void setJackard(String value){
-        lJackard.setText(value);
-    }
     public void setClusterNum(Integer value){
         lClusterNum.setText(value.toString());
-    }
-    public void setDice(String value){
-        lDice.setText(value);
-    }
-
-    public void setWekaTime(Float time){
-        lWekaTime.setText(time.toString() + " sec");
-    }
-
-    public void setOwnTime(Float time){
-        lOwnTime.setText(time.toString() + " sec");
     }
 
 
     public enum Position{ LEFT, RIGHT }
+
+    private enum StatsComboBox{
+        PROPERTIES("Właściwości"),
+        CLUSTERS("Klastry"),
+        INITIAL("Punkty początkowe"),
+        OTHERS("Inne");
+
+        final String value;
+
+        private StatsComboBox(String value){
+            this.value = value;
+        }
+
+        public static JComboBox<String> getJComboBox(){
+            return new JComboBox<>(new String[]{PROPERTIES.value, CLUSTERS.value,INITIAL.value, OTHERS.value});
+        }
+    }
 }
