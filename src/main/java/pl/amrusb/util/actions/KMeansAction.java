9ba8cbf97ac2f2740eb2263f1939f59d84bac1c2
Bartosz Surma <bartosz.surma@outlook.com>
2024-03-04 18:18:14 +0100

    Zamkniecie segmentacji w akcji

--- src/main/java/pl/amrusb/util/actions/KMeansAction.java
+++ src/main/java/pl/amrusb/util/actions/KMeansAction.java
@@ -1,19 +1,28 @@
 package pl.amrusb.util.actions;
 
-import pl.amrusb.util.threads.KMeansThread;
+import pl.amrusb.algs.seg.IKMeans;
+import pl.amrusb.algs.seg.imp.KMeans;
 import pl.amrusb.util.ui.MainFrame;
 import pl.amrusb.util.ui.panels.BottomPanel;
 import pl.amrusb.util.ui.ClusterInputDialog;
 import pl.amrusb.util.ui.MainMenuBar;
 import pl.amrusb.util.ui.panels.ImagePanel;
 
-import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class KMeansAction implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
         ImagePanel current = (ImagePanel) MainFrame.getTabbedPane().getSelectedComponent();
         BottomPanel.setDurationInfoVisible(false);
         if (current.hasSegmentedImage()) {
@@ -28,12 +37,33 @@ public class KMeansAction implements ActionListener {
         Integer clusterNum = dialog.getClusterCount();
         Boolean original = dialog.checkImageSource();
 
+
+
         if (clusterNum != null) {
-            new KMeansThread(
-                    original,
-                    clusterNum,
-                    (JMenuItem) e.getSource()
-            ).start();
+            current.setCursor( Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+            AtomicReference<IKMeans> segmentation = new AtomicReference<>();
+            Future<?> segmenationExec = executor.submit(()->{
+                if(original){
+                    segmentation.set(new KMeans(clusterNum, current.getOriginalImage()));
+                }
+                else{
+                    segmentation.set(new KMeans(clusterNum, current.getRescaledImage()));
+                }
+                segmentation.get().execute();
+            });
+
+            executor.shutdown();
+
+            try {
+                segmenationExec.get();
+                BufferedImage output = segmentation.get().getOutputImage();
+                current.setImageLabel(output);
+                current.setSegmentedImage(output);
+
+                current.setCursor(Cursor.getDefaultCursor());
+            } catch (InterruptedException | ExecutionException ex) {
+                throw new RuntimeException(ex);
+            }
         }
     }
 }
