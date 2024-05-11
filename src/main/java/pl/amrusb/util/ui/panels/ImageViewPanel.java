c1e079e351f6344eceb98dc001953273f09d3cec
Bartosz Surma <bartosz.surma@outlook.com>
2024-05-11 15:44:01 +0200

    Okno podglądu zdjęcia

--- src/main/java/pl/amrusb/util/ui/panels/ImageViewPanel.java
+++ src/main/java/pl/amrusb/util/ui/panels/ImageViewPanel.java
@@ -1,12 +1,23 @@
 package pl.amrusb.util.ui.panels;
 
 import lombok.Getter;
+import lombok.SneakyThrows;
+import pl.amrusb.ui.MainFrame;
+import pl.amrusb.ui.MainMenuBar;
 import pl.amrusb.util.constants.AlgorithmsMetrics;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
+
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 
 public class ImageViewPanel extends JPanel {
     @Getter
@@ -31,6 +42,20 @@ public class ImageViewPanel extends JPanel {
 
         this.add(lAlgorithm,BorderLayout.NORTH);
         this.add(spImage, BorderLayout.CENTER);
+
+
+        lImage.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                ImagePreviewer dialog = new ImagePreviewer();
+                dialog.show(image, name.getValue());
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                lImage.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+            }
+        });
     }
 
     public ImageViewPanel(String name){
@@ -51,6 +76,19 @@ public class ImageViewPanel extends JPanel {
 
         this.add(lAlgorithm,BorderLayout.NORTH);
         this.add(spImage, BorderLayout.CENTER);
+
+        lImage.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                ImagePreviewer dialog = new ImagePreviewer();
+                dialog.show(image, name);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                lImage.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+            }
+        });
     }
 
     public void setImage(String html){
@@ -59,5 +97,53 @@ public class ImageViewPanel extends JPanel {
 
     public void setImage(BufferedImage image){
         this.image = image;
+    }
+
+
+    private class ImagePreviewer{
+        @SneakyThrows
+        private void show(BufferedImage image, String what){
+            JDialog dMain = new JDialog();
+            JLabel lImage = new JLabel();
+
+
+            String htmlString = "<html><img src=\"file:%s\" width=\"%s\" height=\"%s\"></html>";
+            int width = image.getWidth();
+            int height = image.getHeight();
+            double frameWidth = MainFrame.getFrameWidth();
+            double frameHeight = MainFrame.getFrameHeight();
+
+            if (width >= frameWidth || height >= frameHeight) {
+                double scale = Math.min(frameWidth / (width), frameHeight / (height));
+                width = (int)(width * scale);
+                height = (int)(height * scale);
+            }
+
+
+            File stream;
+            stream = File.createTempFile("show-img", ".jpg");
+            ImageIO.write(image, "jpg", stream);
+            htmlString = String.format(htmlString, stream , width, height);
+            dMain.setTitle("Podgląd: " + what );
+            dMain.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+            dMain.setSize(width, height);
+            dMain.setResizable(false);
+            dMain.setLocationRelativeTo(MainMenuBar.getOwner());
+
+            lImage.setText(htmlString);
+
+            dMain.add(lImage);
+
+            dMain.setVisible(true);
+
+            dMain.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+                        dMain.dispose();
+                    }
+                }
+            });
+        }
     }
 }
